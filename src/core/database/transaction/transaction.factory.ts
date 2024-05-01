3853d3774dc165086be9bcf79f5ea10f46020c05
Taylan YILDIZ <taylanyildz@outlook.com>
2024-05-01 10:55:13 +0300

    feature(typeOrm) Type-ORM Added
    Type Orm Added with all Configuration and Transaction Factory

---
+++ src/core/database/transaction/transaction.factory.ts
@@ -0,0 +1,23 @@
+import { DataSource, EntityManager, QueryRunner } from "typeorm";
+import ITransacion from "./transaction.interface";
+
+export default class TransactionFactory implements ITransacion {
+    constructor(private readonly runner: QueryRunner) { }
+
+
+    manager: EntityManager = this.runner.manager;
+
+
+    start(): Promise<void> {
+        return this.runner.startTransaction();
+    }
+    commit(): Promise<void> {
+        return this.runner.commitTransaction();
+    }
+    rollback(): Promise<void> {
+        return this.runner.rollbackTransaction();
+    }
+    release(): Promise<void> {
+        return this.runner.release();
+    }
+}
